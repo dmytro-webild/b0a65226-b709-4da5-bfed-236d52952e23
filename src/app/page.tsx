46e1e3add056f49087d43778ac6c3567d785dae6
bender <kudin.dima@gmail.com>
2026-04-30 19:52:24 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function LandingPage() {
         { id: "c1", imageSrc: "http://img.b2bpic.net/free-photo/soft-pastel-hues-room-kids_23-2151690358.jpg" },
         { id: "c2", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3D5csfS2eM56HgjLvoOJW5Mm44W/uploaded-1777578546831-esmkhuyd.jpg" },
         { id: "c3", imageSrc: "http://img.b2bpic.net/free-photo/confetti-made-circle-center-put-party_23-2147593120.jpg" },
-        { id: "c4", imageSrc: "http://img.b2bpic.net/free-photo/kids-teenagers-birthday-party_23-2147783758.jpg" },
+        { id: "c4", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3D5csfS2eM56HgjLvoOJW5Mm44W/uploaded-1777578699890-mxrrboe1.jpg" },
         { id: "c5", imageSrc: "http://img.b2bpic.net/free-photo/balloons-triangle-shape-paper-yellow-background_23-2147889765.jpg" },
         { id: "c6", imageSrc: "http://img.b2bpic.net/free-photo/close-up-dinosaurs-animal-figure-toys-sweet-candy-sprinkles_53876-41760.jpg" },
       ]}
